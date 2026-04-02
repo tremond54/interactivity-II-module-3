150419af201b8e239693040f4b051a55efb2816c
tremond54 <tremond54@gmail.com>
2026-04-02 09:30:08 -0400

    Change message color to purple on button click

--- script2.js
+++ script2.js
@@ -3,4 +3,5 @@ const button = document.getElementById("thisButton");
 button.addEventListener("click", function() {
   document.getElementById("message").innerText = "You clicked the button!";
   document.body.style.backgroundColor = "lightblue";
+  document.getElementById("message").style.color = "purple";
 });
