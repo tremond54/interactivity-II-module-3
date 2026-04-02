656485c6285d25ad76f391ae629fa08831ba16f1
tremond54 <tremond54@gmail.com>
2026-04-02 09:21:17 -0400

    Add button click event to change message and background

---
+++ script2.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("thisButton");
+
+button.addEventListener("click", function() {
+  document.getElementById("message").innerText = "You clicked the button!";
+  document.body.style.backgroundColor = "lightblue";
+});
